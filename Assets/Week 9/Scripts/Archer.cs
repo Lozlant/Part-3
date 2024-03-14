1abb61fa0efcb6aeed41c116d922f8180ec430df
Lozlant <Lozlant@outlook.com>
2024-03-14 14:25:14 -0400

    Task1 completed: archer and merchant made

---
+++ Assets/Week 9/Scripts/Archer.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Archer : Villager
+{
+    public GameObject ArrowPrefab;
+    public Transform SpawnPoint;
+    public float delay = 0.25f;
+
+    protected override void Attack()
+    {
+        destination = transform.position;
+        base.Attack();
+        Invoke("SpawnArrow", delay);
+    }
+    void SpawnArrow()
+    {
+        Instantiate(ArrowPrefab, SpawnPoint.position, SpawnPoint.rotation);
+    }
+}
