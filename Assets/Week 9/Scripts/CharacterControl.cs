def33486d9652fc41f8ee5c39289fd1ce1238ddc
Lozlant <Lozlant@outlook.com>
2024-03-14 15:58:15 -0400

    Task4b completed: display currently selected type

--- Assets/Week 9/Scripts/CharacterControl.cs
+++ Assets/Week 9/Scripts/CharacterControl.cs
@@ -2,10 +2,22 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CharacterControl : MonoBehaviour
 {
+    public TextMeshProUGUI villagerTypeLable;
     public static Villager SelectedVillager { get; private set; }
+
+    private void Update()
+    {
+        if (SelectedVillager == null)
+        {
+            villagerTypeLable.text = "No Selection";
+            return;
+        }
+        villagerTypeLable.text = SelectedVillager.GetType().ToString();
+    }
     public static void SetSelectedVillager(Villager villager)
     {
         if(SelectedVillager != null)
